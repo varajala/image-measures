48b7c5befea38c834348a9a0aa41f47c6bbbcb02
varajala <valtteri.rajalainen@gmail.com>
2022-11-03 19:36:34 +0200

    Added line drawing

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,43 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
+enum DrawMode {
+    Reference,
+    Measure,
+}
+
+interface IPoint {
+    readonly x: number;
+    readonly y: number;
+}
+
+interface IContext {
+    dc: CanvasRenderingContext2D | null;
+    imageWidth: number;
+    imageHeight: number;
+    referenceStart: IPoint;
+    referenceEnd: IPoint;
+    referenceLength: number;
+    measureStart: IPoint;
+    measureEnd: IPoint;
+}
+
+const context: IContext = {
+    dc: null,
+    imageWidth: 0,
+    imageHeight: 0,
+    referenceStart: { x: 0, y: 0 },
+    referenceEnd: { x: 0, y: 0 },
+    referenceLength: 0,
+    measureStart: { x: 0, y: 0 },
+    measureEnd: { x: 0, y: 0 },
+};
+
 const Application: React.FunctionComponent = () => {
     const [canvasElement, setCanvasElement] = React.useState<HTMLCanvasElement | null>(null);
     const [filePickerElement, setFilePickerElement] = React.useState<HTMLInputElement | null>(null);
     const [selectedImage, setSelectedImage] = React.useState<HTMLImageElement | null>(null);
+    const [drawMode, setDrawMode] = React.useState<DrawMode>(DrawMode.Reference);
 
     React.useEffect(() => {
         const canvas: HTMLElement | undefined = document.getElementById("display");
@@ -26,13 +59,36 @@ const Application: React.FunctionComponent = () => {
 
         canvasElement.width = selectedImage.width;
         canvasElement.height = selectedImage.height;
+        context.imageWidth = selectedImage.width;
+        context.imageHeight = selectedImage.height;
 
-        const drawContext = canvasElement.getContext("2d");
-        drawContext.drawImage(selectedImage, 0, 0);
+        context.dc = canvasElement.getContext("2d");
+        context.dc.drawImage(selectedImage, 0, 0);
         canvasElement.toDataURL("image/png");
+        canvasElement.addEventListener("click", startLine);
     }, [selectedImage]);
 
-    function updateImageSelection(event: React.SyntheticEvent<HTMLButtonElement>) {
+    function startLine(event: MouseEvent) {
+        console.log(context);
+        context.dc.drawImage(selectedImage, 0, 0);
+        context.referenceStart = { x: event.x, y: event.y };
+        canvasElement.removeEventListener("click", startLine);
+        canvasElement.addEventListener("click", endLine);
+    }
+
+    function endLine(event: MouseEvent) {
+        console.log(context);
+        context.referenceEnd = { x: event.x, y: event.y };
+        context.dc.beginPath();
+        context.dc.moveTo(context.referenceStart.x, context.referenceStart.y);
+        context.dc.lineTo(event.x, event.y);
+        context.dc.stroke();
+
+        canvasElement.removeEventListener("click", endLine);
+        canvasElement.addEventListener("click", startLine);
+    }
+
+    function updateImageSelection() {
         const files = filePickerElement.files;
         if (files.length === 0) {
             return;
@@ -46,14 +102,19 @@ const Application: React.FunctionComponent = () => {
             setSelectedImage(image);
         };
         fileReader.readAsDataURL(imageFile);
-
     }
 
+    const hideImageSelection = selectedImage !== null;
+    const hideDrawingInputs = selectedImage === null;
     return (
         <div>
-            <div>
+            <div hidden={hideImageSelection}>
                 <input type={"file"} id={"filePicker"} accept={"image/png"}></input>
                 <button onClick={updateImageSelection}>Load</button>
+            </div>
+            <div hidden={hideDrawingInputs}>
+                <button onClick={() => setDrawMode(DrawMode.Reference)}>Reference</button>
+                <button onClick={() => setDrawMode(DrawMode.Measure)}>Measure</button>
             </div>
             <canvas id={"display"}></canvas>
         </div>
