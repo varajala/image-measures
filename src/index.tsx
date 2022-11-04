8c2f128524795d48019e7c0a9574c3bb9c565b63
varajala <valtteri.rajalainen@gmail.com>
2022-11-04 13:52:25 +0200

    Added measure calculation

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,6 @@ interface IContext {
     imageHeight: number;
     referenceStart: IPoint;
     referenceEnd: IPoint;
-    referenceLength: number;
     measureStart: IPoint;
     measureEnd: IPoint;
 }
@@ -30,7 +29,6 @@ const emptyContext: IContext = {
     imageHeight: 0,
     referenceStart: { x: 0, y: 0 },
     referenceEnd: { x: 0, y: 0 },
-    referenceLength: 0,
     measureStart: { x: 0, y: 0 },
     measureEnd: { x: 0, y: 0 },
 };
@@ -45,12 +43,25 @@ function drawLine(context: CanvasRenderingContext2D, start: IPoint, end: IPoint,
         context.closePath();
 }
 
+function calculateLength(a: IPoint, b: IPoint): number {
+    const dx = Math.abs(a.x - b.x);
+    const dy = Math.abs(a.y - b.y);
+    return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+}
+
+function parsePositiveInteger(input: string): number {
+    const num = Number.parseInt(input);
+    return Number.isNaN(num) ? 0 : Math.abs(num);
+}
+
 const Application: React.FunctionComponent = () => {
     const [canvasElement, setCanvasElement] = React.useState<HTMLCanvasElement | null>(null);
     const [filePickerElement, setFilePickerElement] = React.useState<HTMLInputElement | null>(null);
+    const [referenceLengthInputElement, setReferenceLengthInputElement] = React.useState<HTMLInputElement | null>(null);
     const [selectedImage, setSelectedImage] = React.useState<HTMLImageElement | null>(null);
     const [drawMode, setDrawMode] = React.useState<DrawMode>(DrawMode.ReferenceStart);
     const [context, setContext] = React.useState<IContext>(emptyContext);
+    const [measureResult, setMeasureResult] = React.useState<number>(0);
 
     React.useEffect(() => {
         const canvas: HTMLElement | undefined = document.getElementById("display");
@@ -59,10 +70,14 @@ const Application: React.FunctionComponent = () => {
         }
 
         const filePicker: HTMLElement | undefined = document.getElementById("filePicker");
-        if (canvas) {
+        if (filePicker) {
             setFilePickerElement(filePicker as HTMLInputElement);
         }
 
+        const referenceLengthInput: HTMLElement | undefined = document.getElementById("referenceLengthInput");
+        if (referenceLengthInput) {
+            setReferenceLengthInputElement(referenceLengthInput as HTMLInputElement);
+        }
     }, []);
 
     React.useEffect(() => {
@@ -92,6 +107,19 @@ const Application: React.FunctionComponent = () => {
         canvasElement.addEventListener("click", handleClick);
         return () => canvasElement.removeEventListener("click", handleClick);
     }, [canvasElement, selectedImage, drawMode, context]);
+
+    function updateMeasure() {
+        const referenceLength = calculateLength(context.referenceStart, context.referenceEnd);
+        const measureLength = calculateLength(context.measureStart, context.measureEnd);
+        const referenceInput = parsePositiveInteger(referenceLengthInputElement.value);
+
+        if (referenceInput === 0 || referenceLength === 0 || measureLength === 0) {
+            setMeasureResult(0);
+        }
+
+        const unitInPixels = referenceInput / referenceLength;
+        setMeasureResult(measureLength * unitInPixels);
+    }
 
     function handleClick(event: MouseEvent) {
         const rect = canvasElement.getBoundingClientRect();
@@ -157,6 +185,9 @@ const Application: React.FunctionComponent = () => {
             <div hidden={hideDrawingInputs}>
                 <button onClick={() => setDrawMode(DrawMode.ReferenceStart)}>Reference</button>
                 <button onClick={() => setDrawMode(DrawMode.MeasureStart)}>Measure</button>
+                <button onClick={() => updateMeasure()}>Calculate</button>
+                <input type="number" min={0} max={Number.MAX_SAFE_INTEGER} id={"referenceLengthInput"}></input>
+                <p>{measureResult}</p>
             </div>
             <canvas id={"display"} style={{ margin: "0px", padding: "0px" }}></canvas>
         </div>
